61914e0badd7630b2331511f6868e4a0d1e7a109
gagikk <gagikk@gmail.com>
2019-05-07 02:16:22 +0400

    * CMakeLists.txt and reformat code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
-#include <secp256k1_rangeproof.h>
 #include <secp256k1.h>
+#include <secp256k1_rangeproof.h>
 
 #include <cryptopp/config.h>
 #ifndef CRYPTOPP_NO_GLOBAL_BYTE
 namespace CryptoPP
 {
-typedef unsigned char byte;
+    typedef unsigned char byte;
 }
 #endif
+
 #include <cryptopp/aes.h>
+#include <cryptopp/filters.h>
 #include <cryptopp/hex.h>
 #include <cryptopp/modes.h>
 #include <cryptopp/osrng.h>
-#include <cryptopp/filters.h>
 #include <cryptopp/sha.h>
+
+
 
 /*
 static auto build_confidential_tx = [](const string &A_, const string &B_, asset val, bool generate_range_proof)
@@ -46,87 +49,86 @@ static auto build_confidential_tx = [](const string &A_, const string &B_, asset
 #define PK_SZ 33
 #define PROOF_SZ 5134
 
-using blind_factor_t = unsigned char [SK_SZ];
-using private_key_t = unsigned char [SK_SZ];
+using blind_factor_t = unsigned char[SK_SZ];
+using private_key_t  = unsigned char[SK_SZ];
 
-using public_key_t = unsigned char [PK_SZ];
-using commitment_t = unsigned char [PK_SZ];
+using public_key_t = unsigned char[PK_SZ];
+using commitment_t = unsigned char[PK_SZ];
 
 using shared_secret_t = unsigned char[CryptoPP::SHA512::DIGESTSIZE];
 
-static secp256k1_context* ctx = secp256k1_context_create( SECP256K1_CONTEXT_VERIFY | SECP256K1_CONTEXT_SIGN );
-static CryptoPP::SHA512 _sha512;
-static CryptoPP::SHA256 _sha256;
+static secp256k1_context *ctx = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY | SECP256K1_CONTEXT_SIGN);
+static CryptoPP::SHA512   _sha512;
+static CryptoPP::SHA256   _sha256;
 
 
 struct __attribute__((__packed__)) Ret
 {
-    public_key_t T;
-    public_key_t P;
+    public_key_t   T;
+    public_key_t   P;
     blind_factor_t B;
-    commitment_t C;
+    commitment_t   C;
     CryptoPP::byte E[CryptoPP::AES::BLOCKSIZE];
-    size_t proof_len;
-    unsigned char commitment_range_proof[PROOF_SZ];
+    size_t         proof_len;
+    unsigned char  commitment_range_proof[PROOF_SZ];
 };
 
-extern "C" {
-int __attribute__((used)) build_confidential_tx( unsigned char * ret, public_key_t A_p, public_key_t B_p, uint64_t value, uint64_t asset, int generate_range_proof);
-uint32_t __attribute__((used, const)) sizeofRet() { return sizeof(Ret); }
-int __attribute__((used)) blinding_sum( blind_factor_t ret, blind_factor_t blinding_factors, uint8_t count, uint8_t non_neg_count);
-void __attribute__((used)) sha256( unsigned char ret[32], unsigned char * data, uint32_t sz);
-void __attribute__((used)) aes_decrypt( CryptoPP::byte * plain_data, CryptoPP::byte * encrypted_data, uint32_t sz, shared_secret_t shared_secret_b);
-int __attribute__((used)) generate_shared_secret( shared_secret_t shared_secret, secp256k1_pubkey pk, private_key_t sk);
+extern "C"
+{
+    int __attribute__((used)) build_confidential_tx(unsigned char *ret, public_key_t A_p, public_key_t B_p, uint64_t value, uint64_t asset, int generate_range_proof);
+    uint32_t __attribute__((used, const)) sizeofRet( ) { return sizeof(Ret); }
+    int __attribute__((used)) blinding_sum(blind_factor_t ret, blind_factor_t blinding_factors, uint8_t count, uint8_t non_neg_count);
+    void __attribute__((used)) sha256(unsigned char ret[32], unsigned char *data, uint32_t sz);
+    void __attribute__((used)) aes_decrypt(CryptoPP::byte *plain_data, CryptoPP::byte *encrypted_data, uint32_t sz, shared_secret_t shared_secret_b);
+    int __attribute__((used)) generate_shared_secret(shared_secret_t shared_secret, secp256k1_pubkey pk, private_key_t sk);
 }
 
-int generate_shared_secret( shared_secret_t shared_secret, secp256k1_pubkey pk, private_key_t sk)
+int generate_shared_secret(shared_secret_t shared_secret, secp256k1_pubkey pk, private_key_t sk)
 {
-    int ok = 1;
-    size_t sz = PK_SZ;
+    int          ok = 1;
+    size_t       sz = PK_SZ;
     public_key_t _pk;
-    ok &= secp256k1_ec_pubkey_tweak_mul( ctx, &pk, sk);
-    ok &= secp256k1_ec_pubkey_serialize( ctx, _pk, &sz, &pk, SECP256K1_EC_COMPRESSED);
+    ok &= secp256k1_ec_pubkey_tweak_mul(ctx, &pk, sk);
+    ok &= secp256k1_ec_pubkey_serialize(ctx, _pk, &sz, &pk, SECP256K1_EC_COMPRESSED);
 
-    _sha512.CalculateDigest( shared_secret, &_pk[1], PK_SZ - 1);
+    _sha512.CalculateDigest(shared_secret, &_pk[1], PK_SZ - 1);
 
     return ok;
 }
 
-void sha256( unsigned char ret[32], unsigned char * data, uint32_t sz)
+void sha256(unsigned char ret[32], unsigned char *data, uint32_t sz)
 {
-    _sha256.CalculateDigest( ret, data, sz);
+    _sha256.CalculateDigest(ret, data, sz);
 }
 
-void aes_decrypt( CryptoPP::byte * plain_data, CryptoPP::byte * encrypted_data, uint32_t sz, shared_secret_t shared_secret_b)
+void aes_decrypt(CryptoPP::byte *plain_data, CryptoPP::byte *encrypted_data, uint32_t sz, shared_secret_t shared_secret_b)
 {
     CryptoPP::CBC_Mode<CryptoPP::AES>::Decryption aes;
-    aes.SetKeyWithIV( shared_secret_b, 32, &shared_secret_b[32]);
-    aes.ProcessData( plain_data, encrypted_data, sz);
+    aes.SetKeyWithIV(shared_secret_b, 32, &shared_secret_b[32]);
+    aes.ProcessData(plain_data, encrypted_data, sz);
 }
 
-static
-int blind( commitment_t commitment, blind_factor_t blind_factor, uint64_t value)
+static int blind(commitment_t commitment, blind_factor_t blind_factor, uint64_t value)
 {
-    int ok = 1;
+    int                           ok = 1;
     secp256k1_pedersen_commitment _commit;
-    ok &= secp256k1_pedersen_commit( ctx, &_commit, blind_factor, value, secp256k1_generator_h);
-    ok &= secp256k1_pedersen_commitment_serialize( ctx, commitment, &_commit);
+    ok &= secp256k1_pedersen_commit(ctx, &_commit, blind_factor, value, secp256k1_generator_h);
+    ok &= secp256k1_pedersen_commitment_serialize(ctx, commitment, &_commit);
     return ok;
 }
 
-static
-int blind( commitment_t commitment, blind_factor_t blind_factor, uint64_t blind_tweak, uint64_t value)
+static int blind(commitment_t commitment, blind_factor_t blind_factor, uint64_t blind_tweak, uint64_t value)
 {
     int ok = 1;
     if(!blind_tweak)
         return 0;
 
     blind_factor_t blind_tweak_;
-    memset( blind_tweak_, 0, sizeof(blind_tweak_));
-    memcpy( blind_tweak_, &blind_tweak, sizeof(blind_tweak));
+    memset(blind_tweak_, 0, sizeof(blind_tweak_));
+    memcpy(blind_tweak_, &blind_tweak, sizeof(blind_tweak));
 
-    ok &= secp256k1_ec_privkey_tweak_mul( ctx, blind_factor, blind_tweak_);
-    ok &= blind( commitment, blind_factor, value);
+    ok &= secp256k1_ec_privkey_tweak_mul(ctx, blind_factor, blind_tweak_);
+    ok &= blind(commitment, blind_factor, value);
 
     return ok;
 }
@@ -135,40 +137,39 @@ int blinding_sum(blind_factor_t ret, blind_factor_t blinding_factors, uint8_t co
 {
     int ok = 1;
 
-    unsigned char * p_blinding_factors[count]; // using VLA, so we limit the count to max 255
-    for( uint8_t i = 0; i < count; ++i)
+    unsigned char *p_blinding_factors[count]; // using VLA, so we limit the count to max 255
+    for(uint8_t i = 0; i < count; ++i)
         p_blinding_factors[i] = &blinding_factors[i];
-    ok &= secp256k1_pedersen_blind_sum( ctx, ret, p_blinding_factors, count, non_neg_count);
+    ok &= secp256k1_pedersen_blind_sum(ctx, ret, p_blinding_factors, count, non_neg_count);
 
-    return  ok;
+    return ok;
 }
 
-static
-int range_proof_sign( unsigned char proof[PROOF_SZ], size_t* proof_len, uint64_t min_value, commitment_t commit, const blind_factor_t commit_blind,  const blind_factor_t nonce, int8_t base10_exp, uint8_t min_bits, uint64_t actual_value)
+static int range_proof_sign(unsigned char proof[PROOF_SZ], size_t *proof_len, uint64_t min_value, commitment_t commit, const blind_factor_t commit_blind, const blind_factor_t nonce, int8_t base10_exp, uint8_t min_bits, uint64_t actual_value)
 {
-    int ok = 1;
+    int ok     = 1;
     *proof_len = PROOF_SZ;
 
     secp256k1_pedersen_commitment _commit;
-    ok &= secp256k1_pedersen_commitment_parse( ctx, &_commit, commit);
-    ok &= secp256k1_rangeproof_sign( ctx, proof, proof_len,
-                                     min_value,
-                                     &_commit,
-                                     commit_blind,
-                                     nonce,
-                                     base10_exp,
-                                     min_bits,
-                                     actual_value,
-                                     nullptr, 0, nullptr, 0,
-                                     secp256k1_generator_h);
+    ok &= secp256k1_pedersen_commitment_parse(ctx, &_commit, commit);
+    ok &= secp256k1_rangeproof_sign(ctx, proof, proof_len,
+                                    min_value,
+                                    &_commit,
+                                    commit_blind,
+                                    nonce,
+                                    base10_exp,
+                                    min_bits,
+                                    actual_value,
+                                    nullptr, 0, nullptr, 0,
+                                    secp256k1_generator_h);
     return ok;
 }
 
 
-int build_confidential_tx( unsigned char * ret, public_key_t A_p, public_key_t B_p, uint64_t value, uint64_t asset, int generate_range_proof)
+int build_confidential_tx(unsigned char *ret, public_key_t A_p, public_key_t B_p, uint64_t value, uint64_t asset, int generate_range_proof)
 {
     int ok = 1;
-    memset( ret, 0, sizeofRet());
+    memset(ret, 0, sizeofRet( ));
 
     if(!asset)
         return 1;
@@ -180,69 +181,68 @@ int build_confidential_tx( unsigned char * ret, public_key_t A_p, public_key_t B
     size_t sz = PK_SZ;
 
     private_key_t tx_key_s;
-    public_key_t tx_key_p;
+    public_key_t  tx_key_p;
 
-    rng.GenerateBlock( tx_key_s, sizeof(tx_key_s));
+    rng.GenerateBlock(tx_key_s, sizeof(tx_key_s));
 
-    ok &= secp256k1_ec_pubkey_create( ctx, &_tx_key_p, tx_key_s);
-    ok &= secp256k1_ec_pubkey_serialize( ctx, tx_key_p, &sz, &_tx_key_p, SECP256K1_EC_COMPRESSED);
+    ok &= secp256k1_ec_pubkey_create(ctx, &_tx_key_p, tx_key_s);
+    ok &= secp256k1_ec_pubkey_serialize(ctx, tx_key_p, &sz, &_tx_key_p, SECP256K1_EC_COMPRESSED);
 
-    CryptoPP::byte nonce[ CryptoPP::SHA256::DIGESTSIZE ];
-    _sha256.CalculateDigest( nonce, tx_key_s, SK_SZ);
+    CryptoPP::byte nonce[CryptoPP::SHA256::DIGESTSIZE];
+    _sha256.CalculateDigest(nonce, tx_key_s, SK_SZ);
 
 
-    ok &= secp256k1_ec_pubkey_parse( ctx, &_A_p, A_p, sizeof(public_key_t));
+    ok &= secp256k1_ec_pubkey_parse(ctx, &_A_p, A_p, sizeof(public_key_t));
     shared_secret_t shared_secret_a;
-    ok &= generate_shared_secret( shared_secret_a, _A_p, tx_key_s);
+    ok &= generate_shared_secret(shared_secret_a, _A_p, tx_key_s);
 
 
     blind_factor_t addr_blind;
-    _sha256.CalculateDigest( addr_blind, shared_secret_a, sizeof (shared_secret_a));
+    _sha256.CalculateDigest(addr_blind, shared_secret_a, sizeof(shared_secret_a));
 
     secp256k1_pubkey _P_p;
-    public_key_t P_p;
+    public_key_t     P_p;
 
-    ok &= secp256k1_ec_pubkey_parse( ctx, &_P_p, B_p, sizeof(public_key_t));
-    ok &= secp256k1_ec_pubkey_tweak_add( ctx, &_P_p, addr_blind);
-    ok &= secp256k1_ec_pubkey_serialize( ctx, P_p, &sz, &_P_p, SECP256K1_EC_COMPRESSED);
+    ok &= secp256k1_ec_pubkey_parse(ctx, &_P_p, B_p, sizeof(public_key_t));
+    ok &= secp256k1_ec_pubkey_tweak_add(ctx, &_P_p, addr_blind);
+    ok &= secp256k1_ec_pubkey_serialize(ctx, P_p, &sz, &_P_p, SECP256K1_EC_COMPRESSED);
 
-    ok &= secp256k1_ec_pubkey_parse( ctx, &_B_p, B_p, sizeof(public_key_t));
+    ok &= secp256k1_ec_pubkey_parse(ctx, &_B_p, B_p, sizeof(public_key_t));
     shared_secret_t shared_secret_b;
-    ok &= generate_shared_secret( shared_secret_b, _B_p, tx_key_s);
+    ok &= generate_shared_secret(shared_secret_b, _B_p, tx_key_s);
 
 
     CryptoPP::CBC_Mode<CryptoPP::AES>::Encryption aes;
-    aes.SetKeyWithIV( shared_secret_b, 32, &shared_secret_b[32]);
+    aes.SetKeyWithIV(shared_secret_b, 32, &shared_secret_b[32]);
 
     CryptoPP::byte encrypted_data[CryptoPP::AES::BLOCKSIZE];
     CryptoPP::byte plain_data[CryptoPP::AES::BLOCKSIZE];
-    memset( plain_data, 0, sizeof (plain_data));
-    memcpy( plain_data, &value, sizeof (value));
-    aes.ProcessData( encrypted_data, plain_data, sizeof (plain_data));
+    memset(plain_data, 0, sizeof(plain_data));
+    memcpy(plain_data, &value, sizeof(value));
+    aes.ProcessData(encrypted_data, plain_data, sizeof(plain_data));
 
     blind_factor_t amount_blind;
-    _sha256.CalculateDigest( amount_blind, shared_secret_b, sizeof (shared_secret_b));
+    _sha256.CalculateDigest(amount_blind, shared_secret_b, sizeof(shared_secret_b));
 
 
     commitment_t commitment;
     ok &= blind(commitment, amount_blind, asset, value);
 
-    size_t proof_len = 0;
+    size_t        proof_len = 0;
     unsigned char commitment_range_proof[PROOF_SZ];
-    memset( commitment_range_proof, 0, PROOF_SZ);
+    memset(commitment_range_proof, 0, PROOF_SZ);
     if(generate_range_proof)
-        ok &= range_proof_sign( commitment_range_proof, &proof_len, 0, commitment, amount_blind, nonce, 0, 0, value);
+        ok &= range_proof_sign(commitment_range_proof, &proof_len, 0, commitment, amount_blind, nonce, 0, 0, value);
 
     Ret result;
-    memcpy( &result.T, tx_key_p, sizeof(public_key_t));
-    memcpy( &result.P, P_p, sizeof(public_key_t));
-    memcpy( &result.B, amount_blind, sizeof(blind_factor_t));
-    memcpy( &result.C, commitment, sizeof(commitment_t));
-    memcpy( &result.E, encrypted_data, sizeof(encrypted_data));
+    memcpy(&result.T, tx_key_p, sizeof(public_key_t));
+    memcpy(&result.P, P_p, sizeof(public_key_t));
+    memcpy(&result.B, amount_blind, sizeof(blind_factor_t));
+    memcpy(&result.C, commitment, sizeof(commitment_t));
+    memcpy(&result.E, encrypted_data, sizeof(encrypted_data));
     result.proof_len = proof_len;
-    memcpy( &result.commitment_range_proof, commitment_range_proof, proof_len);
+    memcpy(&result.commitment_range_proof, commitment_range_proof, proof_len);
 
-    memcpy( ret, &result, sizeof(Ret));
+    memcpy(ret, &result, sizeof(Ret));
     return (0 == ok);
 }
-
